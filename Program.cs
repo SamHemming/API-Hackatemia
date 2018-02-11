2a85bf5a5de2182d4b05db4c7e29f5e0d15a7472
SamHemming <samhem@metropolia.fi>
2018-02-11 19:27:37 +0200

    The initial commit
    Program.cs has the code in it and all the other files are just adds and settings.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+
+namespace API_Hackatemia
+{
+    class Program
+    {
+        public Program() //the Constructor function
+        {
+        }
+
+        static void Main() //the main functions
+        {
+            Console.WriteLine("Write the name to search: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+
+            string data = Program.GetRequest(name);
+            Console.WriteLine(data);
+        }
+
+        ///Makes GetRequest and returns the data(json)
+        public static string GetRequest(string name)
+        {
+            string api = "https://newsapi.org/v2/everything?"; //the url of the api
+            string apikey = "a93471e18f9f40b186f8e64bc9d48a86"; //The api key for newsapi.org
+
+            string totalUrl = $"{api}q={name}&language=en&apiKey={apikey}";
+
+            string json = new WebClient().DownloadString(totalUrl);
+
+            return json;
+        }
+    }
+}
